31e91d57811f435d0de0852a27f79aa3446967bf
anjanaanil.mec@gmail.com <anjanaanil.mec@gmail.com>
2023-01-08 01:38:17 +0530

    code updates for project

--- lesson-4-deploying-terraform-assets/exercises/starter/zone1/main.tf
+++ lesson-4-deploying-terraform-assets/exercises/starter/zone1/main.tf
@@ -23,3 +23,11 @@ locals {
      "kubernetes.io/role/elb" = 1
    }
  }
+
+ module "project_alb" {
+   source             = "./modules/alb"
+   ec2                = module.project_ec2.aws_instance
+   ec2_sg             = module.project_ec2.ec2_sg
+   subnet_id          = module.vpc.public_subnet_ids
+   vpc_id             = module.vpc.vpc_id
+ }
